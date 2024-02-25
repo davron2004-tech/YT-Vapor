3bc301f8b074db88f776602ae0fec67c1886ef24
Davron Abdukhakimov <davron0506150@gmail.com>
2024-02-26 02:04:52 +0500

    chnages

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,21 +7,32 @@ import Vapor
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        var tlsConfig: TLSConfiguration = .makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
 
-    app.databases.use(
-        DatabaseConfigurationFactory.postgres(
-            configuration: .init(
-                hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-                port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-                username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-                password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-                database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-                tls: .prefer(try .init(configuration: .clientDefault)))),
-        as: .psql
-    )
+        var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+        postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
+
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        // ...
+    }
+//    app.databases.use(
+//        DatabaseConfigurationFactory.postgres(
+//            configuration: .init(
+//                hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+//                port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+//                username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+//                password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+//                database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+//                tls: .prefer(try .init(configuration: .clientDefault)))),
+//        as: .psql
+//    )
 
     app.migrations.add(CreateSongs())
-    //try await app.autoMigrate()
+    try await app.autoMigrate()
 
     // register routes
     try routes(app)
