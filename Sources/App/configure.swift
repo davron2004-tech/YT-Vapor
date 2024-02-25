be77730d155663b84e9a130d206d6d6b2a276170
Davron Abdukhakimov <davron0506150@gmail.com>
2024-02-25 17:49:53 +0500

    localchange

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -11,11 +11,11 @@ public func configure(_ app: Application) async throws {
     app.databases.use(
         DatabaseConfigurationFactory.postgres(
             configuration: .init(
-                hostname: Environment.get("dpg-cndht56n7f5s73ble00g-a") ?? "localhost",
-                port: Environment.get("5432").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-                username: Environment.get("yt_vapor_database_user") ?? "vapor_username",
-                password: Environment.get("YCsLsZPlr320gf7BlR1b8maok67JvKk2") ?? "vapor_password",
-                database: Environment.get("yt_vapor_database") ?? "vapor_database",
+                hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+                port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+                username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+                password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+                database: Environment.get("DATABASE_NAME") ?? "vapor_database",
                 tls: .prefer(try .init(configuration: .clientDefault)))),
         as: .psql
     )
