7178ba0b2de26820944d907e295be2e1ebabc2e7
Davron Abdukhakimov <davron0506150@gmail.com>
2024-02-26 02:07:31 +0500

    changes1

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,14 +8,7 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     if let databaseURL = Environment.get("DATABASE_URL") {
-        var tlsConfig: TLSConfiguration = .makeClientConfiguration()
-        tlsConfig.certificateVerification = .none
-        let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
-
-        var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
-        postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
-
-        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+        try app.databases.use(.postgres(url: databaseURL), as: .psql)
     } else {
         // ...
     }
